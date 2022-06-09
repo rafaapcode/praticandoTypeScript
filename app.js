e09fe38d11f02e28e7b20d4ffb0fb5fd7201b3bb
rafaapcode <rafaap2003@gmail.com>
2022-06-09 12:14:03 -0300

    teste

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
 let btn = document.getElementById("btn");
-function adicionarNumeros(n1, n2) {
+function adicionarNumeros(n1, n2, devePrintar, frase) {
+    let result = n1 + n2;
+    if (devePrintar) {
+        console.log(frase + result);
+    }
     return n1 + n2;
 }
+let frase = "O resultado é : ";
+let devePrintar = true;
 btn.addEventListener("click", () => {
-    console.log(adicionarNumeros(Number(input1.value), Number(input2.value)));
+    console.log(adicionarNumeros(Number(input1.value), Number(input2.value), devePrintar, frase));
 });
--- app.ts
+++ app.ts
@@ -2,12 +2,21 @@ let input1 = document.getElementById("input1") as HTMLInputElement;
 let input2 = document.getElementById("input2") as HTMLInputElement;
 let btn = document.getElementById("btn") as HTMLButtonElement;
 
-function adicionarNumeros(n1: number, n2: number) {
+function adicionarNumeros(n1: number, n2: number, devePrintar: boolean, frase:string) {
+    let result = n1 + n2;
+
+    if(devePrintar){
+        console.log(frase + result);
+    }
+    
     return n1 + n2;
 }
 
+let frase = "O resultado é : ";
+let devePrintar = true;
+
 btn.addEventListener("click", () => {
 
-    console.log(adicionarNumeros(Number(input1.value), Number(input2.value)));
+    console.log(adicionarNumeros(Number(input1.value), Number(input2.value), devePrintar, frase));
 
 });

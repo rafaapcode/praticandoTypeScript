d985e560c882d8d7dec9f55330a9fad4abf6f15d
rafaapcode <rafaap2003@gmail.com>
2022-06-09 11:15:14 -0300

    start

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var input1 = document.getElementById("input1");
-var input2 = document.getElementById("input2");
-var btn = document.getElementById("btn");
+"use strict";
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let btn = document.getElementById("btn");
 function adicionarNumeros(n1, n2) {
     return n1 + n2;
 }
-btn.addEventListener("click", function () {
+btn.addEventListener("click", () => {
     console.log(adicionarNumeros(Number(input1.value), Number(input2.value)));
 });
